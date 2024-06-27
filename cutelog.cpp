c7ba884bf708853c5552f07e01d3bfbee749681e
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-06-27 12:10:38 +0800

    update.

--- cutelog.cpp
+++ cutelog.cpp
@@ -1,35 +1,64 @@
-#include "cutelog.h"
+#pragma once
 
 #include <concepts>
+#include <cstdint>
 #include <format>
 #include <iostream>
 #include <source_location>
-#include <string>
-#include <string_view>
 
-using std::cout;
-using std::endl;
-using namespace cutelog;
+namespace cutelog {
 
-// void CuteLog(std::string_view str, std::source_location loc = std::source_location::current()) {
-//     cout << std::format("{}:{}:{}:{}:{}", loc.file_name(), loc.function_name(), loc.line(),
-//                         loc.column(), str)
-//          << endl;
-// }
+// 宏定义与宏展开技术
+#define CUTELOG_FOREACH_LOG_LEVEL(f) \
+    f(TRACE) f(DEBUG) f(INFO) f(CRITICAL) f(WARNING) f(ERROR) f(FATAL)
 
-template <typename T>
-concept Incrementable = requires(T x) {
-    x++;
-    ++x;
+enum class LogLevel : uint8_t {
+// 注意这里 name 后的逗号,
+// TRACE,
+// DUBUG,
+// ...
+#define _FUNCTION(name) name,
+    // 宏展开, 替换所有_FUNCTION
+    CUTELOG_FOREACH_LOG_LEVEL(_FUNCTION)
+#undef _FUNCTION
 };
 
-template <Incrementable T>
-void process(const T& t) {}
-
-int main() {
-    // CuteLog("hello");
-    Incrementable auto value1{1};
-    // std::string str1 = "dsadsa"s;
-    LogInfo("dsadsa, {}", 43);
-    return 0;
+std::string LogLevelName(LogLevel lev) {
+    switch (lev) {
+#define _FUNCTION(name)  \
+    case LogLevel::name: \
+        return #name;  // # 返回字符串 name -> "name"
+        CUTELOG_FOREACH_LOG_LEVEL(_FUNCTION);
+#undef _FUNCTION
+    }
+    return "unknown";
 }
+
+template <typename T>
+struct with_source_location {
+private:
+    T inner;
+    std::source_location loc;
+
+public:
+    template <typename U>
+        requires std::constructible_from<T, U>
+    consteval with_source_location(U &&inner,
+                                   std::source_location loc = std::source_location::current())
+        : inner(std::forward<U>(inner)), loc(std::move(loc)) {}
+    constexpr const T &format() const {
+        return inner;
+    }
+    constexpr const std::source_location &location() const {
+        return loc;
+    }
+};
+
+template <typename... Args>
+void Log(with_source_location<std::format_string<Args...>> fmt, Args &&...args) {
+    const auto &loc = fmt.location();
+    std::cout << loc.file_name() << ":" << loc.line() << " [Info]"
+              << std::vformat(fmt.format().get(), std::make_format_args(args...)) << "\n";
+}
+
+}  // namespace cutelog
---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <concepts>
+#include <format>
+#include <iostream>
+#include <source_location>
+#include <string>
+#include <string_view>
+
+using std::cout;
+using std::endl;
+
+void CuteLog(std::string_view str, std::source_location loc = std::source_location::current()) {
+    cout << std::format("{}:{}:{}:{}:{}", loc.file_name(), loc.function_name(), loc.line(),
+                        loc.column(), str)
+         << endl;
+}
+
+template <typename T>
+concept Incrementable = requires(T x) {
+    x++;
+    ++x;
+};
+
+template <Incrementable T>
+void process(const T& t) {}
+
+int main() {
+    // CuteLog("hello");
+    Incrementable auto value1{1};
+    // std::string str1 = "dsadsa"s;
+    return 0;
+}
